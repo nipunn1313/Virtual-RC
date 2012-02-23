991264aa2a07202788c4023d18ae7bf2bb94f6a6
Robert Liu <rql@andrew.cmu.edu>
2012-02-23 17:49:34 -0500

    Temporary commit before merge

--- tracker.py
+++ tracker.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#! /usr/bin/python
+
+import sys
 
 from opencv.cv import *
 from opencv.highgui import *
@@ -6,7 +8,10 @@ import pdb
 
 from FPSLogger import *
 
+#Mouse Callbacks
+
 printcolor = True;
+
 def HSVMouseCallback(event, x, y, flags, hsv_frame):
     if (printcolor and event == CV_EVENT_MOUSEMOVE):
         color = hsv_frame[y][x];
@@ -16,46 +21,67 @@ def BGRMouseCallback(event, x, y, flags, rgb_frame):
         color = rgb_frame[y][x];
         print "BGR: " + str(color);
 
+#Main
 
-print "Initializing the Tracker";
+if __name__ == '__main__':
 
-#Initialize
-white = cvScalar(255,255,255,0);
-size = cvSize(640, 480);
-hsv_frame = cvCreateImage(size, IPL_DEPTH_8U, 3);
+    print "Initializing the Tracker";
 
-capture = cvCreateCameraCapture(-1);
-if not capture:
-    print "Could not open webcam";
-    exit(1);
-frame = cvQueryFrame(capture);
+    #Initialize
+    WHITE = cvScalar(255,255,255,0);
+    SIZE = cvSize(640, 480);
 
-cvNamedWindow("Webcam", CV_WINDOW_AUTOSIZE);
-cvNamedWindow("HSV_Webcam", CV_WINDOW_AUTOSIZE);
+    # Capture webcam
+    try:
+        # try to get the device number from the command line
+        device = int (sys.argv [1])
 
-cvSetMouseCallback("Webcam", BGRMouseCallback, frame);
-cvSetMouseCallback("HSV_Webcam", HSVMouseCallback, hsv_frame);
+        # got it ! so remove it from the arguments
+        del sys.argv [1]
+    except (IndexError, ValueError):
+        # no device number on the command line, assume we want the 1st device
+        device = -1
 
-print "Successfully Initialized the Tracker";
+    # Access to the webcam stream
+    capture = cvCreateCameraCapture(device);
 
-#Now to do the fun stuff!
-fps = FPSLogger();
-while 1:
-    #Query and show frame
-    frame = cvQueryFrame(capture);
-    if frame == None:
-        print "Yikes. Frame is None. Aborting"
+    if not capture:
+        print "Could not open webcam";
         exit(1);
+    
+    # Create windows
+    cvNamedWindow("Webcam", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("HSV_Webcam", CV_WINDOW_AUTOSIZE);
 
-    #Convert BGR -> HSV
-    cvCvtColor(frame, hsv_frame, CV_BGR2HSV);
+    # Grab buffer of original image
+    frame = cvQueryFrame(capture);
+    # Create buffer for the hsv image
+    hsv_frame = cvCreateImage(SIZE, IPL_DEPTH_8U, 3);
 
-    #Show images. 
-    #WaitKey magically flushes buffer? Dunno why.
-    cvShowImage("Webcam", frame);
-    cvShowImage("HSV_Webcam", hsv_frame);
-    cvWaitKey(1);
+    # Set up mouse callbacks
+    cvSetMouseCallback("Webcam", BGRMouseCallback, frame);
+    cvSetMouseCallback("HSV_Webcam", HSVMouseCallback, hsv_frame);
 
-    #Measure rate
-    #fps.printFPS();
+    print "Successfully Initialized the Tracker";
+
+    #Now to do the fun stuff!
+    fps = FPSLogger();
+    while 1:
+        #Query and show frame
+        frame = cvQueryFrame(capture);
+        if frame == None:
+            print "Yikes. Frame is None. Aborting"
+            exit(1);
+
+        #Convert BGR -> HSV
+        cvCvtColor(frame, hsv_frame, CV_BGR2HSV);
+
+        #Show images.
+        #WaitKey magically flushes buffer? Dunno why.
+        cvShowImage("Webcam", frame);
+        cvShowImage("HSV_Webcam", hsv_frame);
+        cvWaitKey(1);
+
+        #Measure rate
+        #fps.printFPS();
 
