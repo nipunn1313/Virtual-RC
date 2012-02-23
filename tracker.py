5ceddf7c95a2122ac21e431424ee272eb3d76b88
Nipunn Koorapati <nipunn1313@gmail.com>
2012-02-23 17:43:31 -0500

    Added some optical flow stuff

--- tracker.py
+++ tracker.py
@@ -19,27 +19,40 @@ def BGRMouseCallback(event, x, y, flags, rgb_frame):
 
 print "Initializing the Tracker";
 
-#Initialize
-white = cvScalar(255,255,255,0);
-size = cvSize(640, 480);
-hsv_frame = cvCreateImage(size, IPL_DEPTH_8U, 3);
-
+#Open Camera!
 capture = cvCreateCameraCapture(-1);
 if not capture:
     print "Could not open webcam";
     exit(1);
 frame = cvQueryFrame(capture);
 
-cvNamedWindow("Webcam", CV_WINDOW_AUTOSIZE);
-cvNamedWindow("HSV_Webcam", CV_WINDOW_AUTOSIZE);
+#Allocate!
+white = cvScalar(255,255,255,0);
+blocksize = (7,7);
+size = cvGetDims(frame);
+size = (size[1], size[0]); #It's backwards. Dunno why.
+#BitDepth && number of channesl
+hsv = cvCreateImage(size, IPL_DEPTH_8U, 3);
 
-cvSetMouseCallback("Webcam", BGRMouseCallback, frame);
-cvSetMouseCallback("HSV_Webcam", HSVMouseCallback, hsv_frame);
+velx = cvCreateImage(size, IPL_DEPTH_32F, 1);
+vely = cvCreateImage(size, IPL_DEPTH_32F, 1);
+
+#Make Window!
+camWindow = "Webcam"
+flowWindow = "Flow"
+grayWindow = "Gray Image"
+cvNamedWindow(camWindow, CV_WINDOW_AUTOSIZE);
+cvNamedWindow(flowWindow, CV_WINDOW_AUTOSIZE);
+cvNamedWindow(grayWindow, CV_WINDOW_AUTOSIZE);
+
+#Customize Window!
+#cvSetMouseCallback(camWindow, BGRMouseCallback, frame);
 
 print "Successfully Initialized the Tracker";
 
 #Now to do the fun stuff!
 fps = FPSLogger();
+image = None
 while 1:
     #Query and show frame
     frame = cvQueryFrame(capture);
@@ -48,12 +61,38 @@ while 1:
         exit(1);
 
     #Convert BGR -> HSV
-    cvCvtColor(frame, hsv_frame, CV_BGR2HSV);
+    #cvCvtColor(frame, hsv_frame, CV_BGR2HSV);
+
+    if image is None:
+        image = cvCreateImage(size, IPL_DEPTH_8U, 3);
+        gray = cvCreateImage(size, IPL_DEPTH_8U, 1);
+        gray_prev = cvCreateImage(size, IPL_DEPTH_8U, 1);
+        pyramid = cvCreateImage(size, IPL_DEPTH_8U, 1);
+        prev_pyramid = cvCreateImage(size, IPL_DEPTH_8U, 1);
+        eig = cvCreateImage(size, IPL_DEPTH_32F, 1);
+        temp = cvCreateImage(size, IPL_DEPTH_32F, 1);
+        points = [[],[]]
+
+    cvCopy(frame, image)
+
+    gray_prev = gray
+    cvCvtColor(image, gray, CV_BGR2GRAY);
+
+    #Do optical flow calculation
+    cvCalcOpticalFlowLK(gray_prev, gray, blocksize, velx, vely);
+
+    scribble = cvCreateImage(size, IPL_DEPTH_8U, 3)
+    cvCvtColor(gray, scribble, cv.CV_GRAY2BGR)
+    for y in range(0, size[1], 8):
+        for x in range(0, size[0], 8):
+            cvLine(scribble, (x, y), (int(x+velx[y,x]), int(y + vely[y,x])), (0,255,0))
 
     #Show images. 
     #WaitKey magically flushes buffer? Dunno why.
-    cvShowImage("Webcam", frame);
-    cvShowImage("HSV_Webcam", hsv_frame);
+    cvShowImage(camWindow, frame);
+    cvShowImage(grayWindow, gray);
+    cvShowImage(flowWindow, scribble);
+
     cvWaitKey(1);
 
     #Measure rate
