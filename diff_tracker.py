80e7cd2e99ee782f3841a41689c395d317ce22c2
Nipunn Koorapati <nipunn1313@gmail.com>
2012-02-23 19:24:37 -0500

    Optical flow works!

--- diff_tracker.py
+++ diff_tracker.py
@@ -53,22 +53,18 @@ if __name__ == '__main__':
     
     #Allocate!
     white = cvScalar(255,255,255,0);
-    blocksize = (7,7);
     size = cvGetDims(frame);
     size = (size[1], size[0]); #It's backwards. Dunno why.
+
     #BitDepth && number of channesl
-    hsv = cvCreateImage(size, IPL_DEPTH_8U, 3);
-    
-    velx = cvCreateMat(size[1], size[0], CV_32FC1);
-    vely = cvCreateMat(size[1], size[0], CV_32FC1);
+    prev_frame = cvCreateImage(size, IPL_DEPTH_8U, 3);
+    diff_frame = cvCreateImage(size, IPL_DEPTH_8U, 3);
     
     #Make Window!
     camWindow = "Webcam"
-    flowWindow = "Flow"
-    grayWindow = "Gray Image"
+    diffWindow = "Diff"
     cvNamedWindow(camWindow, CV_WINDOW_AUTOSIZE);
-    cvNamedWindow(flowWindow, CV_WINDOW_AUTOSIZE);
-    cvNamedWindow(grayWindow, CV_WINDOW_AUTOSIZE);
+    cvNamedWindow(diffWindow, CV_WINDOW_AUTOSIZE);
     
     #Customize Window!
     #cvSetMouseCallback(camWindow, BGRMouseCallback, frame);
@@ -77,58 +73,25 @@ if __name__ == '__main__':
     
     #Now to do the fun stuff!
     fps = FPSLogger();
-    image = None
     while 1:
+        cvCopy(frame, prev_frame);
+
         # Query and show frame
         frame = cvQueryFrame(capture);
         if frame == None:
             print "Yikes. Frame is None. Aborting"
             exit(1);
     
+        cvSub(frame, prev_frame, diff_frame);
+
         #Show images.
-        #WaitKey magically flushes buffer? Dunno why.
-        cvShowImage("Webcam", frame);
-        #cvShowImage("HSV_Webcam", hsv);
-        cvWaitKey(1);
-    
-        #Convert BGR -> HSV
-        #cvCvtColor(frame, hsv, CV_BGR2HSV);
-    
-        if image is None:
-            image = cvCreateImage(size, IPL_DEPTH_8U, 3);
-            gray = cvCreateImage(size, IPL_DEPTH_8U, 1);
-            gray_prev = cvCreateImage(size, IPL_DEPTH_8U, 1);
-            pyramid = cvCreateImage(size, IPL_DEPTH_8U, 1);
-            prev_pyramid = cvCreateImage(size, IPL_DEPTH_8U, 1);
-            eig = cvCreateImage(size, IPL_DEPTH_32F, 1);
-            temp = cvCreateImage(size, IPL_DEPTH_32F, 1);
-            points = [[],[]]
-    
-        # copy the frame so we can draw on it
-        cvCopy(frame, image)
-    
-        #gray_prev = gray
-        cvCopy(gray, gray_prev);
-        cvCvtColor(image, gray, CV_BGR2GRAY);
-    
-        # Do optical flow calculation
-        cvCalcOpticalFlowLK(gray_prev, gray, blocksize, velx, vely);
-    
-        scribble = cvCreateImage(size, IPL_DEPTH_8U, 3)
-        cvCvtColor(gray, scribble, cv.CV_GRAY2BGR)
-        for y in range(0, size[1], 8):
-            for x in range(0, size[0], 8):
-                cvLine(scribble, (x, y), (int(x+velx[y,x]), int(y + vely[y,x])),
-                       (0,255,0))
-    
-        # Show images. 
         cvShowImage(camWindow, frame);
-        cvShowImage(grayWindow, gray);
-        cvShowImage(flowWindow, scribble);
-    
-        # WaitKey magically flushes buffer? Dunno why.
+        cvShowImage(diffWindow, diff_frame);
+
+        #WaitKey magically flushes buffer? Dunno why.
         cvWaitKey(1);
     
-            #Measure rate
-            #fps.printFPS();
+    
+        #Measure rate
+        #fps.printFPS();
     
